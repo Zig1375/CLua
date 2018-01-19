6494592416753c4c294abe823a5b1285028f4f8a
zig <zig1375@gmail.com>
2018-01-19 15:04:35 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@ import PackageDescription
 
 let package = Package(
     name: "CLua",
-    pkgConfig: "lua",
+    pkgConfig: "lua5.1",
     providers: [
-        .Brew("Lua"),
-        .Apt("lua5.3")
+        .Brew("lua@5.1"),
+        .Apt("liblua5.1")
     ]
 )
--- shim.h
+++ shim.h
@@ -1,5 +1,5 @@
 #define LUA_COMPAT_APIINTCASTS 1
 
-#import "/usr/local/include/lua.h"
-#import "/usr/local/include/lauxlib.h"
-#import "/usr/local/include/lualib.h"
+#import "/usr/local/include/lua-5.1/lua.h"
+#import "/usr/local/include/lua-5.1/lauxlib.h"
+#import "/usr/local/include/lua-5.1/lualib.h"
